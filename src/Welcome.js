ce6b26aa2909cd02ca97adf14985bec9368871df
dhanoosh <dthirunavukkarasu@scu.edu>
2023-03-19 23:50:05 -0700

    changed img to svg and updated presentation borders

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,19 +1,21 @@
-import dpImage from './images/dp.png';
+import dpImage from './images/dp.svg';
 
 const Welcome = () => {
     return(
-            <div className="welcome-container">
-                <div className="welcome-image-container">
+        <div className="welcome-container">
+            <div className="welcome-image-container">
+                <div className="welcome-image-circle">
                     <img src={dpImage} alt="Download CV" className="welcome-image" />
                 </div>
-                <div className="welcome-text-container">
-                    <p className="welcome-text">Hello There! This is my digital canvas, where I paint a picture of my talents, skills, and projects.</p>
-                    <a href='https://drive.google.com/file/d/1yZWabCfpcbeoK1ij146dCZAsR3YGJ4rj/view?usp=share_link' download target="_blank" rel="noreferrer">
-                        <button className="welcome-button">DOWNLOAD CV</button>
-                    </a>
-                </div>
             </div>
+            <div className="welcome-text-container">
+                <p className="welcome-text">Hello There! This is my digital canvas, where I paint a picture of my talents, skills, and projects.</p>
+                <a href='https://drive.google.com/file/d/1yZWabCfpcbeoK1ij146dCZAsR3YGJ4rj/view?usp=share_link' download target="_blank" rel="noreferrer">
+                    <button className="welcome-button">DOWNLOAD CV</button>
+                </a>
+            </div>
+        </div>
     )
 }
 
-export default Welcome
+export default Welcome;

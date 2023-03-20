4596e28e6a78d4f3a5abcd7a2f8a4187942449a4
dhanoosh <dthirunavukkarasu@scu.edu>
2023-03-20 00:25:22 -0700

    Added QnA bot presentation

--- src/Presentation.js
+++ src/Presentation.js
@@ -4,22 +4,23 @@ const Presentation = () => {
     return (
         <section>
       <h2 className='title'>Presentation</h2>
-        <div className="video-container">
-            <div className="video-box">
-            <iframe
-                    width="560"
-                    height="315" 
-                    src="https://www.youtube.com/embed/7p9TzlqbqZc" 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    allowfullscreen>
-                </iframe>
-                <div className="video-info">
-                    <h2>QnABot</h2>
-                    <p>Learn how to make QnA bot in simple and elegant steps</p>
+            <div className="video-container">
+                <div className="video-box">
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/SUxNCxwmm3U"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen>
+
+                    </iframe>
+                    <div className="video-info">
+                        <h2>QnABot</h2>
+                        <p>Learn how to make QnA bot in simple and elegant steps</p>
+                    </div>
                 </div>
-            </div>
 
             <div className="video-box">
                 <iframe
